bd8e67868e00f24079b64bfb99647e4f37d03470
Alistair Michael <alistair.michael@uq.edu.au>
2023-09-29 10:24:24 +1000

    change output filename

--- lib/compilers/boogie.ts
+++ lib/compilers/boogie.ts
@@ -1,9 +1,14 @@
 import type {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces.js';
 import {BaseCompiler} from '../base-compiler.js';
+import path from 'path';
 
 export class C3Compiler extends BaseCompiler {
     static get key() {
         return 'boogie';
+    }
+
+    override getOutputFilename(inputFilename: string): string {
+        return this.filename(path.dirname(inputFilename) + '/stdout');
     }
 
     override optionsForFilter(filters: ParseFiltersAndOutputOptions, outputFilename: string) {
